69bd7a295f130c1349f83c3bd048bdddf71f0c40
petrov-as <alexander.se.petrov@gmail.com>
2026-06-20 09:12:31 +0300

    Update solution.c

--- problem2-exec-exec-python/solution.c
+++ problem2-exec-exec-python/solution.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 
-// Программе на стандартный поток ввода задается арифметическое выражение
-// в синтаксисе языка python3. Необходимо вычислить это выражение и вывести результат.
-// Использовать дополнительные процессы запрещено — нужно использовать exec.
-
-int main(void) {
-    // TODO: прочитайте выражение из stdin,
-    //       затем вызовите execvp/execlp для запуска python3,
-    //       который вычислит и выведет результат.
-    //       Подсказка: python3 -c "print(<выражение>)"
-
-    return 0;
+int main() {
+    char expr[4096];
+    if (fgets(expr, sizeof(expr), stdin) == NULL) {
+        return 0;   
+    }
+    size_t len = strlen(expr);
+    if (len > 0 && expr[len - 1] == '\n') {
+        expr[len - 1] = '\0';
+    }
+    execlp("python3", "python3", "-c", expr, NULL);
+    perror("execlp python3");
+    return 1;
 }
