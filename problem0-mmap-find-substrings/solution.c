6829d1215b9f2fc41a9a924fac10c2ec19a50eac
petrov-as <alexander.se.petrov@gmail.com>
2026-06-20 09:03:38 +0300

    Update solution.c

--- problem0-mmap-find-substrings/solution.c
+++ problem0-mmap-find-substrings/solution.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,21 +7,51 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-// Программе передаются два аргумента: имя файла и строка для поиска.
-// Необходимо найти все вхождения строки в текстовом файле,
-// используя отображение на память с помощью системного вызова mmap.
-// На стандартный поток вывода вывести список всех позиций (с нуля),
-// где встречается искомая строка, по одной на строку.
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <filename> <search_string>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <filename> <substring>\n", argv[0]);
         return 1;
     }
-
-    // TODO: откройте файл, получите его размер через fstat,
-    //       отобразите в память через mmap,
-    //       найдите все вхождения argv[2] и выведите их позиции
-
+    const char *filename = argv[1];
+    const char *needle = argv[2];
+    size_t needle_len = strlen(needle);
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        perror("fstat");
+        close(fd);
+        return 1;
+    }
+    off_t size = st.st_size;
+    if (size == 0) {
+        close(fd);
+        return 0;
+    }
+    char *data = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        return 1;
+    }
+    if (needle_len == 0) {
+        munmap(data, size);
+        close(fd);
+        return 0;
+    }
+    char *pos = data;
+    int first = 1;
+    while ((pos = memmem(pos, size - (pos - data), needle, needle_len)) != NULL) {
+        if (!first) putchar(' ');
+        printf("%ld", pos - data);
+        first = 0;
+        pos += needle_len;   
+    }
+    putchar('\n');
+    munmap(data, size);
+    close(fd);
     return 0;
 }
