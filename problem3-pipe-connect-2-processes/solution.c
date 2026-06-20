73a517f5e93afefe2dd3318c36b55259fbac6981
petrov-as <alexander.se.petrov@gmail.com>
2026-06-20 09:15:26 +0300

    Update solution.c

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -3,22 +3,51 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-// Программе передаётся два аргумента: CMD1 и CMD2.
-// Необходимо запустить два процесса, выполняющих эти команды,
-// и перенаправить стандартный поток вывода CMD1 на стандартный поток ввода CMD2.
-// Эквивалентно: CMD1 | CMD2
-// Родительский процесс должен завершаться самым последним.
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <CMD1> <CMD2>\n", argv[0]);
+        fprintf(stderr, "Usage: %s CMD1 CMD2\n", argv[0]);
         return 1;
     }
-
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
-
+    char *cmd1 = argv[1];
+    char *cmd2 = argv[2];
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
+    pid_t pid1 = fork();
+    if (pid1 == -1) {
+        perror("fork1");
+        return 1;
+    } else if (pid1 == 0) {
+        close(pipefd[0]);        
+        if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+            perror("dup2");
+            exit(1);
+        }
+        close(pipefd[1]);
+        execlp("/bin/sh", "sh", "-c", cmd1, NULL);
+        perror("execlp cmd1");
+        exit(1);
+    }
+    pid_t pid2 = fork();
+    if (pid2 == -1) {
+        perror("fork2");
+        return 1;
+    } else if (pid2 == 0) {
+        close(pipefd[1]);          
+        if (dup2(pipefd[0], STDIN_FILENO) == -1) {
+            perror("dup2");
+            exit(1);
+        }
+        close(pipefd[0]);
+        execlp("/bin/sh", "sh", "-c", cmd2, NULL);
+        perror("execlp cmd2");
+        exit(1);
+    }
+    close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
     return 0;
 }
